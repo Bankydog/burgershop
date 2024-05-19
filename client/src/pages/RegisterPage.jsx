b93f231bb20000722a40414fe1ffa1c5b0ac985b
Bank <blankinfear@gmail.com>
2024-05-19 23:04:37 +0700

    fisrt

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function RegisterPage() {
+  return (
+    <div className="h-full w-full flex justify-center items-center">
+      register
+    </div>
+  );
+}
