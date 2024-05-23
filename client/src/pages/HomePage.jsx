b3aec864a60daea8dae1ad88e107eb853040221c
Bank <blankinfear@gmail.com>
2024-05-23 12:35:15 +0700

    feat: setting pages

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function HomePage() {
   return (
-    <div className="h-full w-full flex justify-center items-center">error</div>
+    <div className="h-screen w-screen flex justify-center items-center">
+      home
+    </div>
   );
 }
