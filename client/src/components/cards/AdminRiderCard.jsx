cac7857fa7545ed51cf51c4155f2b2856283e8d4
Bank <blankinfear@gmail.com>
2024-10-17 02:53:54 +0700

    feat:add responsive to adminrider page

--- client/src/components/cards/AdminRiderCard.jsx
+++ client/src/components/cards/AdminRiderCard.jsx
@@ -8,11 +8,11 @@ export default function AdminRiderCard({
   handleCancelOrder,
 }) {
   return (
-    <div className="grid w-full grid-cols-1 gap-4 mx-auto md:grid-cols-4">
+    <div className="grid w-full grid-cols-1 gap-4 mx-auto md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {orders.map((order) => (
         <div
           key={order.order_no}
-          className="w-full max-w-lg p-4 bg-orange-100 border border-black rounded"
+          className="w-full max-w-lg p-4 bg-orange-100 border border-black rounded-md shadow-md"
         >
           <div className="flex flex-col md:flex-row">
             {/* Content - Left Column */}
@@ -37,22 +37,21 @@ export default function AdminRiderCard({
               </div>
             </div>
             {/* Content - Right Column */}
-            <div className="w-[200px] h-[115px] mt-4 flex flex-col items-center justify-center place-self-center">
+            <div className="w-[100px] h-[100px] mt-4 flex flex-col items-center justify-center place-self-center">
               <img
                 src={`../public/img/${order.state}.png`}
                 alt={order.state}
-                className="object-cover w-[100px] h-[100px] rounded"
+                className="object-cover w-full h-full rounded"
               />
-              <p className="mt-2 text-xl">State: {order.state}</p>
             </div>
           </div>
           <div className="flex justify-center mt-2 mb-2">
             <p className="text-xl font-bold text-rose-500">{order.comment}</p>
           </div>
           {/* Buttons */}
-          <div className="grid grid-cols-2 gap-3 mt-3 text-2xl md:grid-cols-2 place-items-center">
+          <div className="grid grid-cols-2 gap-3 mt-3 text-xl md:grid-cols-4 place-items-center">
             <button
-              className={`w-[100px] h-[40px] border-2 border-black rounded-lg text-white ${
+              className={`w-full h-[40px] border-2 border-black rounded-lg text-white ${
                 ["sending", "sended"].includes(order.state)
                   ? "bg-gray-400 cursor-not-allowed"
                   : "bg-yellow-500 hover:bg-yellow-400 hover:text-slate-100"
@@ -63,7 +62,7 @@ export default function AdminRiderCard({
               Sending
             </button>
             <button
-              className={`w-[100px] h-[40px] bg-blue-500 border-2 border-black rounded-lg text-white  ${
+              className={`w-full h-[40px] bg-blue-500 border-2 border-black rounded-lg text-white ${
                 ["sended"].includes(order.state)
                   ? "bg-gray-400 cursor-not-allowed"
                   : "bg-blue-500 hover:bg-blue-400 hover:text-slate-100"
@@ -74,14 +73,14 @@ export default function AdminRiderCard({
               Sended
             </button>
             <button
-              className="w-[100px] h-[40px] bg-green-500 border-2 border-black rounded-lg text-white hover:bg-green-400 hover:text-slate-100"
+              className="w-full h-[40px] bg-green-500 border-2 border-black rounded-lg text-white hover:bg-green-400 hover:text-slate-100"
               onClick={() => handleFinished(order.order_no)}
               disabled={["cooked", "sending"].includes(order.state)}
             >
               Finish
             </button>
             <button
-              className="w-[100px] h-[40px] bg-red-500 border-2 border-black rounded-lg text-white hover:bg-red-400 hover:text-slate-100"
+              className="w-full h-[40px] bg-red-500 border-2 border-black rounded-lg text-white hover:bg-red-400 hover:text-slate-100"
               onClick={() => handleCancelOrder(order.order_no)}
             >
               Cancel
