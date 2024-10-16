620f122371fbd844662e7485314e294c8eee238c
Bank <blankinfear@gmail.com>
2024-10-17 01:40:00 +0700

    feat:add responsive homepage admin

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,16 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px", // Default Tailwind breakpoint
+      md: "768px", // Default Tailwind breakpoint
+      lg: "1024px", // Default Tailwind breakpoint
+      xl: "1280px", // Default Tailwind breakpoint
+      "2xl": "1536px", // Default Tailwind breakpoint
+
+      // Custom screen size
+      smm: "350px", // => @media (min-width: 350px) { ... }
+    },
   },
   plugins: [],
 };
